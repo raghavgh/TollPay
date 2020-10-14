b045fe592f244260b4cfaa2a2aafa6063c9ee31f
Raghav Paliwal <paliwal.raghav9352@gmail.com>
2020-10-14 17:01:32 +0530

    Add more packages

--- src/main/java/com/example/Tollpay/Controller/HomeController.java
+++ src/main/java/com/example/Tollpay/Controller/HomeController.java
@@ -4,7 +4,6 @@ import com.example.Tollpay.Log.Log;
 import com.example.Tollpay.dto.Welcome;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
