e907c0c74de4709c61bf688873ae3feb43048a6a
Raghav Paliwal <paliwal.raghav9352@gmail.com>
2020-10-14 17:06:56 +0530

    Add more functionlity

--- src/main/java/com/example/Tollpay/Controller/AuthenticationController.java
+++ src/main/java/com/example/Tollpay/Controller/AuthenticationController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("authentication")
 public class AuthenticationController {
+
     @PostMapping("welcome")
     public String registerNewUser(){
         Log.print("Welcome ");
